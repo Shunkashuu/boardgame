749a6f3957d98b601b5bd46513cecd7b0451b2db
Sora <sora78700@gmail.com>
2020-07-17 12:21:24 +0200

    add the switch between trainers and pokemon
    when you click on a trainer, they all disapears and pokemon choice appears.

--- assets/js/trainer.js
+++ assets/js/trainer.js
@@ -18,6 +18,8 @@ class Trainer {
   }
 
 //Récupère l'élément html img
+let trainer = document.getElementById("trainer");
+let pokemon = document.getElementById("pokemon");
 let Lucas = document.getElementById("lucas");
 let Victor = document.getElementById("victor");
 let Aurore = document.getElementById("aurore");
@@ -25,22 +27,45 @@ let Gloria = document.getElementById("gloria");
 
 
 //Déclare un dresseur pour la classe trainer
+let p1 = new Trainer("","");
+let p1test = p1;
 const lucas = new Trainer("Lucas","h");
 const victor = new Trainer("Victor","h");
 const aurore = new Trainer("Aurore","f");
 const gloria = new Trainer("Gloria","f");
 
 //Ajoute un évènement de sélèction, et l'affiche dans la console (vérification séléction)
-Lucas.addEventListener('click',function(){console.log(lucas.decrire())})
-Victor.addEventListener('click',function(){console.log(victor.decrire())})
-Aurore.addEventListener('click',function(){console.log(aurore.decrire())})
-Gloria.addEventListener('click',function(){console.log(gloria.decrire())})
+Lucas.addEventListener('click',function(){
+  console.log(lucas.decrire());
+  p1=lucas;
+  hideAndRemove();
+})
+Victor.addEventListener('click',function(){
+  console.log(victor.decrire());
+  p1=victor;
+  hideAndRemove();
+})
+Aurore.addEventListener('click',function(){
+  console.log(aurore.decrire());
+  p1=aurore;
+  hideAndRemove();
+})
+Gloria.addEventListener('click',function(){
+  console.log(gloria.decrire());
+  p1=gloria;
+  hideAndRemove();
+})
 
+
+
+
+//don't show pokémon choice
+//when trainer is chosen, pokémon appears and trainers disapears
+//player 1 can select a pokémon
+//the game begin and the boardgame appears with the trainer and the pokémon chosen
 function hideAndRemove() {
-  while(Trainer = null) {
-    //don't show pokémon choice
-    //when trainer is chosen, pokémon appears and trainers disapears
-    //player 1 can select a pokémon
-    //the game begin and the boardgame appears with the trainer and the pokémon chosen
+  if(p1 !== p1test) {
+    trainer.style.display='none';
+    pokemon.style.display='block';
   }
 }
