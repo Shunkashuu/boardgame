3c6d6cc5be8093fa9d1a617ab5261abe986d810b
Sora <sora78700@gmail.com>
2020-07-18 16:04:22 +0200

    add random objects placement
    add random objetcs placement

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,49 +1,23 @@
 //variables du canvas
-var bw = 600;
-var bh = 600;
+var width =600;
+var height = 600;
 var p = 10;
-var cw = bw + (p*2) + 1;
-var ch = bh + (p*2) + 1;
+var hline = width + (p*2) + 1;
+var vline = height + (p*2) + 1;
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-//appel des pokémon, des "armes" et des obstacles
-var pikachu = new Image();
-    pikachu.src = 'assets/img/pikachu.png';
-    pikachu.addEventListener('load', function() {
-        context.drawImage(pikachu, 20, 20, 50, 50);
-    })
-
-var evoli = new Image();
-    evoli.src = 'assets/img/evoli.png';
-    evoli.addEventListener('load', function() {
-        context.drawImage(evoli, 20, 20, 50, 50);
-    })
-
-var roche = new Image();
-    roche.src = 'assets/img/roche.png';
-    roche.addEventListener('load', function() {
-        context.drawImage(roche, 20, 20, 60, 60);
-    })
-
-var ct_normal = new Image();
-    ct_normal.src = 'assets/img/ct_normal.png';
-    ct_normal.addEventListener('load', function() {
-        context.drawImage(ct_normal, 20, 20, 60, 60);
-    })
-
-
 // Canevas
 function drawBoard(){
-        for (var x = 0; x <= bw; x += 50) {
+        for (var x = 0; x <= width; x += 60) {
             context.moveTo(0.5 + x + p, p);
-            context.lineTo(0.5 + x + p, bh + p);
+            context.lineTo(0.5 + x + p, height + p);
         }
 
-        for (var x = 0; x <= bh; x += 50) {
+        for (var x = 0; x <= height; x += 60) {
             context.moveTo(p, 0.5 + x + p);
-            context.lineTo(bw + p, 0.5 + x + p);
+            context.lineTo(width + p, 0.5 + x + p);
         }
 
         context.strokeStyle = "black";
@@ -51,5 +25,48 @@ function drawBoard(){
     }
 
 drawBoard();
+
+//appel des pokémon, des ct et des obstacles
+var pikachu = new Image();
+    pikachu.src = 'assets/img/pikachu.png';
+    pikachu.addEventListener('load', function() {
+        context.drawImage(pikachu,pikachu_h, pikachu_v, 60, 60);
+})
+
+pikachu_h = getRandomInt(10)*60+p;
+pikachu_v = getRandomInt(10)*60+p;
+
+var evoli = new Image();
+    evoli.src = 'assets/img/evoli.png';
+    evoli.addEventListener('load', function() {
+        context.drawImage(evoli, evoli_h, evoli_v, 60, 60);
+})
+
+evoli_h = getRandomInt(10)*60+p;
+evoli_v = getRandomInt(10)*60+p;
+
+var roche = new Image();
+    roche.src = 'assets/img/roche.png';
+    roche.addEventListener('load', function() {
+        context.drawImage(roche, roche_h, roche_v, 60, 60);
+})
+
+roche_h = getRandomInt(10)*60+p;
+roche_v = getRandomInt(10)*60+p;
+
+var ct_normal = new Image();
+    ct_normal.src = 'assets/img/ct_normal.png';
+    ct_normal.addEventListener('load', function() {
+    context.drawImage(ct_normal,ct_h, ct_v, 60, 60);
+})
+
+ct_h = getRandomInt(10)*60+p;
+ct_v = getRandomInt(10)*60+p;
+
+//fonction pour la position aléatoire des objets
+function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+          
 
 
